2d9854e6bf5e2dedcbbe3c9f3379654dfd204535
Gicbar <camilo2911@gmail.com>
2026-02-23 10:17:17 -0500

    se deja para que sea dinamico el favicon por el logo de la empresa que esta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,40 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import { resolveCompanyFromHost } from "@/lib/supabase/company-resolver"
+import { headers } from "next/headers"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "Gestión Empresarial Inteligente.",
-  description:
-    "Plataforma inteligente para la gestión empresarial multiempresa. Controla inventario, ventas y rentabilidad en tiempo real desde un solo lugar.",
-  generator: "V1.app",
-  icons: {
-    icon: "/favicon.png",
-  },
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers() 
+  const host = headersList.get("host") || ""
+  const company = await resolveCompanyFromHost(host)
+  
+  const iconUrl = company?.logo_url || "/favicon.png"
+
+  if (!company) {
+      return {
+        title: "Gestión Empresarial Inteligente.",
+        description:
+          "Plataforma inteligente para la gestión empresarial multiempresa. Controla inventario, ventas y rentabilidad en tiempo real desde un solo lugar.",
+        generator: "V1.app",
+        icons: {
+          icon: "/favicon.png",
+        },
+      }
+  }
+
+  return {
+    title: `${company.name}`,
+    description: `Gestiona inventario, ventas y rentabilidad de ${company.name} en tiempo real.`,
+    icons: {
+      icon: iconUrl, // 🔥 dinámico
+    },
+  }
 }
+
 
 export default function RootLayout({
   children,
